78d7720aaa0b6d2da617944c006d44989080be1a
Wesley <wesleyosantos91@gmail.com>
2020-09-05 17:25:45 -0300

    refactor: reformulado a estrutura do projeto

--- src/main/java/io/github/wesleyosantos91/backend/apifirst/exception/core/ObjectNotFoundException.java
+++ src/main/java/io/github/wesleyosantos91/backend/apifirst/exception/core/ObjectNotFoundException.java
@@ -10,8 +10,4 @@ public class ObjectNotFoundException extends RuntimeException {
     public ObjectNotFoundException(String message) {
         super(message);
     }
-
-    public ObjectNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
